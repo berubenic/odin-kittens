c625fadf8c0b60f1b77771a538870fe0a2e3d1aa
berubenic <berubenic@gmail.com>
2021-07-27 20:31:26 -0400

    kitten#update

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -25,6 +25,16 @@ class KittensController < ApplicationController
         @kitten = Kitten.find(params[:id])
     end
 
+    def update
+        @kitten = Kitten.find(params[:id])
+
+        if @kitten.update(kitten_params)
+            redirect_to @kitten
+        else
+            render :edit
+        end
+    end
+
     private
         def kitten_params
             params.require(:kitten).permit(:name, :age, :cuteness, :softness)
